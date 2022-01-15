2e3c60dda6bc96854ac8a910e928cd3de39b24a2
semkeamsan <keamsan.sem@gmail.com>
2022-01-15 22:37:02 +0700

    Update SocialiteController.php

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -17,10 +17,10 @@ class SocialiteController extends Controller
     public function callback($provider)
     {
         try {
-            // $user =  Socialite::driver($provider)
-            //     ->setHttpClient(new \GuzzleHttp\Client(['verify' => false]))
-            //     ->user();
-                $user =  Socialite::driver($provider)->stateless()->user();
+            $user =  Socialite::driver($provider)
+                ->setHttpClient(new \GuzzleHttp\Client(['verify' => false]))
+                ->user();
+            //$user =  Socialite::driver($provider)->stateless()->user();
             $auth = SocialAuth::firstOrCreate(['_id' => $user->getId()], [
                 'provider'   => $provider,
                 '_id'   => $user->getId(),
@@ -29,21 +29,20 @@ class SocialiteController extends Controller
                 '_avatar'   => $user->getAvatar(),
             ]);
 
-            if($auth->user){
-                auth()->loginUsingId($auth->user->id,true);
+            if ($auth->user) {
+                auth()->loginUsingId($auth->user->id, true);
                 return redirect()->route('front.account.index');
-            }else{
-              $user = User::firstOrCreate(['email' => $user->getEmail()], [
+            } else {
+                $user = User::firstOrCreate(['email' => $user->getEmail()], [
                     'email'   => $user->getEmail(),
                     'name'   => $user->getName(),
                     'avatar'   => $user->getAvatar(),
                     'email_verified_at' => now(),
                 ]);
                 $auth->update(['user_id' => $user->id]);
-                auth()->loginUsingId($user->id,true);
+                auth()->loginUsingId($user->id, true);
                 return redirect()->route('front.account.index');
             }
-
         } catch (\Throwable $th) {
             throw $th;
         }

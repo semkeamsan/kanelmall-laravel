6100b130e685428747fdf8e3cf7d497e9a095f80
semkeamsan <keamsan.sem@gmail.com>
2022-01-16 16:15:12 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -123,12 +123,15 @@
                                             <span>{{ __('Login with') }}</span>
                                         </div>
                                         <div class="btn-wrapper text-center">
-                                            <button onclick="checkLoginState()" href="{{ route('auth.with','facebook') }}" class="btn btn-neutral btn-icon mb-2">
+                                            <fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
+                                            <a href="{{ route('auth.with', 'facebook') }}"
+                                                class="btn btn-neutral btn-icon mb-2">
                                                 <span class="btn-inner--icon"><img
                                                         src="{{ asset('images/facebook.svg') }}"></span>
                                                 <span class="btn-inner--text">{{ __('Facebook') }}</span>
-                                            </button>
-                                            <a href="{{ route('auth.with','google') }}" class="btn btn-neutral btn-icon mb-2">
+                                            </a>
+                                            <a href="{{ route('auth.with', 'google') }}"
+                                                class="btn btn-neutral btn-icon mb-2">
                                                 <span class="btn-inner--icon"><img
                                                         src="{{ asset('images/google.svg') }}"></span>
                                                 <span class="btn-inner--text">{{ __('Google') }}</span>
@@ -151,35 +154,34 @@
 @endsection
 @push('scripts')
 
-<script>
-     function checkLoginState() {
-        FB.getLoginStatus(function(response) {
-            statusChangeCallback(response);
-        });
-    }
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId: `{{ env('FACEBOOK_APP_ID') }}`,
-            cookie: true, // Enable cookies to allow the server to access the session.
-            xfbml: true, // Parse social plugins on this webpage.
-            version: 'v10.0' // Use this Graph API version for this call.
-        });
-
-        FB.getLoginStatus(function(response) { // Called after the JS SDK has been initialized.
-            statusChangeCallback(response); // Returns the login status.
-        });
-    };
-
-    function statusChangeCallback(response) {
-        if (response.status === 'connected') {
-            FB.api(`me?fields=id,first_name,last_name,gender,birthday,email,friends,picture.width(100).height(100).as(picture_small),picture.width(720).height(720).as(picture_large)`, function(response) {
-                console.log(response);
+    <script>
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
             });
         }
-    }
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId: `{{ env('FACEBOOK_APP_ID') }}`,
+                cookie: true, // Enable cookies to allow the server to access the session.
+                xfbml: true, // Parse social plugins on this webpage.
+                version: 'v12.0' // Use this Graph API version for this call.
+            });
 
+            FB.getLoginStatus(function(response) { // Called after the JS SDK has been initialized.
+                statusChangeCallback(response); // Returns the login status.
+            });
+        };
 
-</script>
-<!-- Load the JS SDK asynchronously -->
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+        function statusChangeCallback(response) {
+            if (response.status === 'connected') {
+                FB.api(`me?fields=id,first_name,last_name,gender,birthday,email,friends,picture.width(100).height(100).as(picture_small),picture.width(720).height(720).as(picture_large)`,
+                    function(response) {
+                        console.log(response);
+                    });
+            }
+        }
+    </script>
+    <!-- Load the JS SDK asynchronously -->
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
 @endpush
